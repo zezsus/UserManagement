cdb96557340358eedff00d30fa667cd95d872b16
Sang <sang166464@huce.edu.vn>
2023-09-15 18:24:25 +0700

    update

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  const history = useHistory();
+
+  const handleDetail = () => {
+    history.push("/detail");
+  };
   return (
     <div>
       <div className="header w-100 bg-black text-white text-center p-5 mb-3">
         <h1>QUẢN LÝ THÀNH VIÊN</h1>
+        <button onClick={handleDetail}>Detai User</button>
       </div>
     </div>
   );
