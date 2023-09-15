3d06dc9615541520db307ecfe127eafb3fbb86ca
Sang <sang166464@huce.edu.vn>
2023-09-15 18:26:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.scss";
-import DetailUser from "./component/DetailUser";
 import Home from "./component/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/detail">
-            <DetailUser />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Home />
     </div>
   );
 }
